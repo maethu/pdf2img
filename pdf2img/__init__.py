5f70f081df13b78c0b133a5b2377a44861695ab7
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2014-06-04 16:47:04 +0200

    Remove special folder generation based sha256

--- pdf2img/__init__.py
+++ pdf2img/__init__.py
@@ -49,18 +49,9 @@ class Pdf2Img(object):
 
         return int(result)
 
-    def destination_folder(self, filehandler):
-        filehandler.seek(0)
-        hash_ = hashlib.sha256(filehandler.read()).hexdigest()
-        destination = '{0}/{1}'.format(self.path, hash_)
-        exists = True
-
-        if not os.path.exists(destination):
-            os.makedirs(destination)
-            exists = False
-
-        filehandler.seek(0)
-        return exists, hash_
+    def destination_folder(self):
+        if not os.path.exists(self.path):
+            os.makedirs(self.path)
 
     def get_resources(self, folder):
         resources = []
@@ -72,18 +63,15 @@ class Pdf2Img(object):
         """Converts a pdf to images using ghostscript"""
         pdf = open(pdf_file, 'r')
         pages = self.count_pages(pdf_file)
-        exists, folder = self.destination_folder(pdf)
+        self.destination_folder()
         resoruces = []
-
-        if exists:
-            return self.get_resources(folder)
 
         if self.limit == -1:
             self.limit = pages
 
         for page in range(1, pages + 1)[:self.limit]:
 
-            output = './var/{0}/{1}_image.png'.format(folder, page)
+            output = '{0}/{1}_image.png'.format(self.path, page)
 
             command = COMMAND_CONVERT_PDF_PAGE[:]
             command.insert(-1, '-dFirstPage={0:d}'.format(page))
@@ -101,7 +89,7 @@ class Pdf2Img(object):
             return_code = process.returncode
             if return_code == 0:
                 print "Ghostscript processed one page of a pdf file."
-                resoruces.append('{0}/{1}_image.png'.format(folder, page))
+                resoruces.append('{0}/{1}_image.png'.format(self.path, page))
             else:
                 print "Ghostscript process did not exit cleanly! "
                 print "Error Code: {0}".format(return_code)
